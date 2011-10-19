5be7fc825b3985f1c2873f915acf73b90f9ed31f
Kamal Bin Mustafa <kamal@smach.net>
2011-10-19 08:41:21 +0800

    setup db

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='myblog',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
+      description='MyBlog - OpenShift App',
+      author='Kamal Mustafa',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Django>=1.3'],
      )
--- wsgi/myblog/settings.py
+++ wsgi/myblog/settings.py
@@ -1,3 +1,4 @@
+import os
 
 ######################
 # MEZZANINE SETTINGS #
@@ -93,9 +94,9 @@ TEMPLATE_LOADERS = (
 DATABASES = {
     "default": {
         # "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "",
+        "ENGINE": "sqlite3",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": os.environ['OPENSHIFT_DATA_DIR'] + '/sqlite3.db',
         # Not used with sqlite3.
         "USER": "",
         # Not used with sqlite3.
