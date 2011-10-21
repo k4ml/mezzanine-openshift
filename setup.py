f4fa947a6d83c1e9137596729f7e0fe925df971c
Kamal Bin Mustafa <kamal@smach.net>
2011-10-22 04:34:52 +0800

    snapshot

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-setup(name='myblog',
-      version='1.0.1',
-      description='MyBlog - OpenShift App',
-      author='Kamal Mustafa',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='myblog',
+    version='1.0.1',
+    description='MyBlog - OpenShift App',
+    author='Kamal Mustafa',
+    author_email='example@example.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=[
+        'Django>=1.3',
+        'mezzanine',
+    ],
+)
