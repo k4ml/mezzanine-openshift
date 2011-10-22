b0063af4c609ce61e73e24a9de47c9452fb19923
Kamal Bin Mustafa <kamal@smach.net>
2011-10-22 15:03:25 +0800

    fix sys.path to libs

--- libs/myblog/manage.py
+++ libs/myblog/manage.py
@@ -6,8 +6,8 @@
 import os
 import sys
 
-wsgi_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
-sys.path.insert(0, wsgi_path)
+libs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, libs_path)
 
 project_path = os.path.dirname(os.path.abspath(__file__))
 project_dir = project_path.split(os.sep)[-1]
